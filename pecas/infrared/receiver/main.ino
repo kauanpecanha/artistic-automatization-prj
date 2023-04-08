83218f4d316042ce0a91ac26aaa23a8b906316cf
ViniciusCMB <108745449+ViniciusCMB@users.noreply.github.com>
2023-04-07 21:17:55 -0300

    Update main.ino

--- pecas/infrared/receiver/main.ino
+++ pecas/infrared/receiver/main.ino
@@ -1,55 +1,52 @@
-#include <Arduino.h>
-#include <IRremote.hpp>
+/* Example of how to use a IR receiver */
 
-const int IR = 9;
+#include <Arduino.h>
+#include <IRremote.hpp> // Library for the InfraRed receiver
+
+// IR receiver parameters 
+const int IR = 9; // pin connected
 IRrecv IrReceiv(IR);
 decode_results result;
 
-const int code1 = 0xAA1;
-const int code2 = 0xAA2;
-const int code3 = 0xAA3;
-const int code4 = 0xAA4;
+const int codeON = 0xAA1;
+const int code1 = 0xAA2;
+const int code2 = 0xAA3;
+const int codeOFF = 0xAA4;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(6, OUTPUT);
-  pinMode(5, OUTPUT);
   IrReceiv.enableIRIn();
 }
 
 void loop()
 {
-  if (IrReceiv.decode(&result))
+  if (IrReceiv.decode(&result)) // If a signal is detected
   {
     Serial.print(result.bits);
     Serial.print(": ");
-    Serial.println(result.value, HEX);
+    Serial.println(result.value, HEX); // Prints the code received
 
     switch (result.value)
     {
+      case codeON:
+
+        break;
       case code1:
-        analogWrite(5, 255);
-        analogWrite(6, 0);
+
         break;
       case code2:
-        analogWrite(5, 0);
-        analogWrite(6, 255);
+
         break;
-      case code3:
-        analogWrite(5, 0);
-        analogWrite(6, 0);
-        break;
-      case code4:
-        analogWrite(5, 104);
-        analogWrite(6, 0);
+      case codeOFF:
+
         break;
 
       default:
         Serial.println("ERROR");
         break;
     }
-    IrReceiv.resume();
+    IrReceiv.resume(); // Clean the receiver for new signals
   }
   delay(1000);
 }
