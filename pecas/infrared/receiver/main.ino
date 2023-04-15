95d49b5c0ebafae935f85c8de3323f5d9b2aa3c3
ViniciusCMB <108745449+ViniciusCMB@users.noreply.github.com>
2023-04-14 22:19:44 -0300

    final update

--- pecas/infrared/receiver/main.ino
+++ pecas/infrared/receiver/main.ino
@@ -11,7 +11,8 @@ decode_results result;
 const int codeON = 0xAA1;
 const int code1 = 0xAA2;
 const int code2 = 0xAA3;
-const int codeOFF = 0xAA4;
+const int code3 = 0xAA4;
+const int codeOFF = 0xAA5;
 
 void setup()
 {
@@ -26,8 +27,9 @@ void loop()
     Serial.print(result.bits);
     Serial.print(": ");
     Serial.println(result.value, HEX); // Prints the code received
-
-    switch (result.value)
+    IrReceiv.resume(); // Clean the receiver for new signals
+  }
+  switch (result.value)
     {
       case codeON:
         // the desired code goes here
@@ -46,7 +48,5 @@ void loop()
         Serial.println("ERROR");
         break;
     }
-    IrReceiv.resume(); // Clean the receiver for new signals
-  }
   delay(1000);
 }
